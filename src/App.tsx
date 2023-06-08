8f096a103e876fcd4bdbed55106e63e5ddbc7b96
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-06-08 15:44:52 -0300

    implementado side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import SideMenu from './components/SideMenu';
 
 function App() {
   return (
     <>
+      <SideMenu />
     </>
   );
 }
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,11 @@
+function SideMenu() {
+  return (
+    <nav>
+      <button>Inicio</button>
+      <button>Cadastros</button>
+      <button>Relatorios</button>
+    </nav>
+  )
+}
+
+export default SideMenu;
